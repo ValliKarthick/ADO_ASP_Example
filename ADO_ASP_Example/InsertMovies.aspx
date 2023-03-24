bb19a8cdcfa1f98149c7542840fb5bde81c28995
vallinannu <vallinannu@LAPTOP-GIVRBI76>
2023-03-24 20:34:04 +0530

    Add project files.

---
+++ ADO_ASP_Example/InsertMovies.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="InsertMovies.aspx.cs" Inherits="ADO_ASP_Example.InsertMovies" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:SqlDataSource runat="server" ID="MySqlSource"
+        ProviderName="System.Data.SqlClient"
+        ConnectionString="Data Source=LAPTOP-GIVRBI76\SQLEXPRESS; Initial Catalog= MovieDetailsDB; Integrated Security=True"
+        SelectCommand="Select * from MovieDetail" />
+    <div>
+        <asp:GridView ID="gv_Movies" runat="server"></asp:GridView>
+    </div>
+    <asp:DetailsView ID="dv_Movie" runat="server" Height="50px" Width="125px" DataSourceID="MySqlSource"></asp:DetailsView>
+    <br />
+    <div>
+        <asp:Table ID="Table1" runat="server" HorizontalAlign="Center">
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lblMovieId" runat="server" Text="ID:"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:TextBox ID="txtMovieId" runat="server"></asp:TextBox>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lblMovieName" runat="server" Text="Name:"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:TextBox ID="txtMovieName" runat="server"></asp:TextBox>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lblGenre" runat="server" Text="Genre:"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:TextBox ID="txtGenre" runat="server"></asp:TextBox>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lblConfirmation" runat="server" Text=""></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:Button ID="btnAdd" runat="server" Text="Add to Database" OnClick="btnAdd_Click" />
+                </asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+    </div>
+
+    <asp:Menu ID="m_AdminAndCustomer" runat="server">
+        <Items>
+            <asp:MenuItem Text="Admin" NavigateUrl="~/Contact.aspx"></asp:MenuItem>
+            <asp:MenuItem Text="Customer" NavigateUrl="~/About.aspx"></asp:MenuItem>
+        </Items>
+    </asp:Menu>
+</asp:Content>
---
+++ ADO_ASP_Example/InsertMovies.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ADO_ASP_Example
+{
+    public partial class InsertMovies : System.Web.UI.Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["MovieDetailsConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadMoviesUsingAdo();
+            //LoadMovies();
+            //LoadMoviesUsingList();
+        }
+        private void LoadMoviesUsingAdo()
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            string querystring = "Select * from MovieDetail";
+            SqlCommand sqlCommand = new SqlCommand(querystring, sqlConnection);
+            try
+            {
+                sqlConnection.Open();
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                gv_Movies.DataSource = sqlDataReader;
+                gv_Movies.DataBind();
+            }
+            catch (SqlException sqlException)
+            {
+                lblConfirmation.Text = sqlException.ToString();
+            }
+            finally
+            {
+                sqlCommand.Dispose();
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+            }
+        }
+        private void LoadMoviesUsingList()
+        {
+            List<MovieModel> moviesList = new List<MovieModel>();
+            MovieModel movie = new MovieModel();
+            movie.MovieId = 1;
+            movie.Name = "ABC";
+            movie.Genre = "Humour";
+            moviesList.Add(movie);
+            gv_Movies.DataSource = moviesList;
+            gv_Movies.DataBind();
+        }
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            string query = "INSERT INTO MovieDetail (MovieId,MovieName,Genre) VALUES(@movieID,@movieName,@genre)";
+
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@movieID", txtMovieId.Text);
+            sqlCommand.Parameters.AddWithValue("@movieName", txtMovieName.Text);
+            sqlCommand.Parameters.AddWithValue("@genre", txtGenre.Text);
+
+            try
+            {
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+                lblConfirmation.Text = "Movie Added to Database Successfully..!";
+                LoadMoviesUsingAdo();
+            }
+            catch (SqlException sqlException)
+            {
+                lblConfirmation.Text = sqlException.ToString();
+            }
+            finally
+            {
+                sqlCommand.Dispose();
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+            }
+        }
+    }
+}
